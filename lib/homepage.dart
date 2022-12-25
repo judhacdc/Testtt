cc4304833857b41c0d87ec0e6b7f71f99a24f9bf
judhacdc <jdxxaa@gmail.com>
2022-12-25 15:48:16 +0700

    Input Text

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -50,22 +50,193 @@ class MyApp extends StatelessWidget {
           ),
           body: TabBarView(
             children: [
-              Center(
-                child: Text(
-                  'Input Data',
-                  style: TextStyle
-                  (fontSize: 40
+             Column(
+              children: [
+                // create input border box
+                Container(
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  height: 54,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.grey[200],
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
                   ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Input NIM',
+                    ),
                   ),
-              ),
-               Center(
-                child: Text(
-                  'List Data',
-                  style: TextStyle
-                  (fontSize: 40
+                ),
+                 Container(
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  height: 54,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.grey[200],
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
                   ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Input Nama',
+                    ),
                   ),
-              ),
+                ),
+                 Container(
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  height: 54,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.grey[200],
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Input Alamat',
+                    ),
+                  ),
+                ), Container(
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  height: 54,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.grey[200],
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Input Nomer Telepon',
+                    ),
+                  ),
+                ),
+                // create dropdown
+                Container(
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  height: 54,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.grey[200],
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: DropdownButton(
+                    isExpanded: true,
+                    underline: SizedBox(),
+                    hint: Text('Jenis Kelamin'),
+                    items: [
+                      DropdownMenuItem(
+                        child: Text('Laki - Laki'),
+                        value: 1,
+                      ),
+                      DropdownMenuItem(
+                        child: Text('Perempuan'),
+                        value: 2,
+                      ),
+                    ],
+                    onChanged: (value) {},
+                  ),
+                ),
+// CREATE BUTTON row
+                Container(
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        height: 54,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.grey[200],
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset: Offset(0, 3), // changes position of shadow
+                            ),
+                          ],
+                        ),
+                        child: FlatButton(
+                          onPressed: () {},
+                          child: Text('Batal'),
+                        ),
+                      ),
+                      Container(
+                        height: 54,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.amber,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset: Offset(0, 3), // changes position of shadow
+                            ),
+                          ],
+                        ),
+                        child: FlatButton(
+                          onPressed: () {},
+                          child: Text('Simpan'),
+                        ),
+                      ),
+                    ],
+                  ),
+             
+                ),
+              ],
+             ),
+
+            Column(
+
+        
+        
+              
+          ),
             ],
           ),
         ),
